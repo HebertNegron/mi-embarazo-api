be22934fb213cf57c753b1b6d7811dabd2814824
HebertNegron <hjnm194@gmail.com>
2024-11-26 11:31:52 -0600

    fix: uncomment try y catch

--- app/src/controllers/auth_router.py
+++ app/src/controllers/auth_router.py
@@ -41,19 +41,19 @@ def login_user(
 
 @auth_router.post("/signup", status_code=status.HTTP_201_CREATED)
 def signup_user(user: UserModel) -> BearerToken:
-    # try:
-    user = auth_service.signup(user)
+    try:
+        user = auth_service.signup(user)
 
-    return BearerToken(
-        access_token=JsonWebTokenTools.create_access_token(user.email),
-        user_email=user.email,
-        user_id=str(user.id),
-        user_name=user.name,
-        role=user.role
-    )
-    # except Exception:
-    #     raise HTTPException(
-    #         status_code=status.HTTP_409_CONFLICT,
-    #         detail="This email address is already in use",
-    #     )
+        return BearerToken(
+            access_token=JsonWebTokenTools.create_access_token(user.email),
+            user_email=user.email,
+            user_id=str(user.id),
+            user_name=user.name,
+            role=user.role
+        )
+    except Exception:
+        raise HTTPException(
+            status_code=status.HTTP_409_CONFLICT,
+            detail="This email address is already in use",
+        )
     
