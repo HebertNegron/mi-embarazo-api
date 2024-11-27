76820f1196d07df35862c14b4cb281fdff40cf57
HebertNegron <hjnm194@gmail.com>
2024-11-27 12:09:40 -0600

    feat: add admin count ep

--- app/src/controllers/__init__.py
+++ app/src/controllers/__init__.py
@@ -3,3 +3,4 @@ from .patients import patients_router
 from .doctors import doctors_router
 from .appointments import appointments_router
 from .users import users_router
+from .dashboard import dashboard_router
--- app/src/services/__init__.py
+++ app/src/services/__init__.py
@@ -2,3 +2,4 @@ from .auth_service import *
 from .user_authentication import *
 from .patients_service import *
 from .doctors_service import *
+from .dashboard_service import *
