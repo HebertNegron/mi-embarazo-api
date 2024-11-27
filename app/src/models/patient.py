3fd0bd88b69f6e9e7e4cc7aeca6bc11bfa98176e
HebertNegron <hjnm194@gmail.com>
2024-11-26 21:23:13 -0600

    fix: patients model

--- app/src/models/patient.py
+++ app/src/models/patient.py
@@ -2,22 +2,22 @@ from pydantic import BaseModel, Field
 from models.pyObjectId import PyObjectId
 
 class PersonalData(BaseModel):
-    name: str
-    gender: str
-    phone: str
-    age: int
-    birthDate: str
-    email: str
-    password: str
-    curp: str
-    maritalStatus: str
-    occupation: str
-    address: dict
+    name: str | None = None
+    gender: str | None = None
+    phone: str | None = None
+    age: int | None = None
+    birthDate: str | None = None
+    email: str | None = None
+    password: str | None = None
+    curp: str | None = None
+    maritalStatus: str | None = None
+    occupation: str | None = None
+    address: dict | None = None
 
 class Patient(BaseModel):
     id: PyObjectId | None = Field(None, alias="_id")
-    record: str
-    name: str
+    record: str | None = None
+    name: str | None = None
     personalData: PersonalData | None = None
     current_phone: str | None = None
     doctor_options: list | None = None
